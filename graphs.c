ef0f058c6ee81c1c09f9750edaef45292f2273b6
Jon Licjon <licjon@protonmail.com>
2023-03-18 07:35:40 -0400

    Move lines with nodes

--- graphs.c
+++ graphs.c
@@ -17,6 +17,7 @@ int main(void)
     Vector2 mousePosition = { -100.0f, -100.0f };
 
     struct node {
+      int id;
       Vector2 position;
       int diameter;
       Color color;
@@ -31,6 +32,7 @@ int main(void)
     int lineIndex = 0;
 
     struct node nodes[MAXNODES];
+    int structCount = 0;
     int nodeIndex = 0;
     int nodeDiameter = 10;
     bool isNodeDrawn = false;
@@ -77,86 +79,123 @@ int main(void)
           }
         }
         //----------------------------------------------------------------------------------
-
+        
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
         
-            ClearBackground(RAYWHITE);
-            
-            bool isSpaceFree = true;
-            bool isMouseOverNode = false;
-            for (int i = 0; i < MAXNODES; i++) {
-              isMouseOverNode = CheckCollisionPointCircle(mousePosition, nodes[i].position, nodeDiameter/2.0);
+        ClearBackground(RAYWHITE);
+        
+        bool isSpaceFree = true;
+        bool isMouseOverNode = false;
+        for (int i = 0; i < MAXNODES; i++) {
+          isMouseOverNode = CheckCollisionPointCircle(
+              mousePosition, nodes[i].position, nodeDiameter / 2.0);
 
-              if (isMouseOverNode && !(isLineStarted || isLineEnded)) {
-                isSpaceFree = false;
-                isNodeDrawn = false;
-              }
-              if (isMouseOverNode && isLineStarted && !isNodeLocked) {
+          if (isMouseOverNode && !(isLineStarted || isLineEnded)) {
+            isSpaceFree = false;
+            isNodeDrawn = false;
+          }
+          if (isMouseOverNode && isLineStarted && !isNodeLocked) {
+            selectedIndex = i;
+            isNodeLocked = true;
+            printf("start node %d coordinates: %f, %f \n", selectedIndex,
+                   nodes[i].position.x, nodes[i].position.y);
+          } else if (isMouseOverNode && isLineEnded && isNodeLocked) {
+            /* startNode = lines[lineIndex].startNode; */
+            selectedIndex = i;
+            printf("end node %d coordinates: %f, %f \n", selectedIndex,
+                   nodes[i].position.x, nodes[i].position.y);
+          }
+
+          for (int j = 0; j < MAXNODES; j++) {
+            if (!CheckCollisionCircles(nodes[i].position, nodeDiameter / 2.0,
+                                       nodes[j].position, nodeDiameter / 2.0)) {
+              if (isMouseOverNode && isNodeSelected && !isNodeLocked) {
+                // Lock node so when moving, selection is kept.
                 selectedIndex = i;
                 isNodeLocked = true;
-                printf("start node %d coordinates: %f, %f \n", selectedIndex, nodes[i].position.x, nodes[i].position.y);
-              } else if (isMouseOverNode && isLineEnded && isNodeLocked) {
-                  /* startNode = lines[lineIndex].startNode; */
-                  selectedIndex = i;
-                  printf("end node %d coordinates: %f, %f \n", selectedIndex, nodes[i].position.x, nodes[i].position.y);
-              }
-
-              for (int j = 0; j < MAXNODES; j++) {
-                if (!CheckCollisionCircles(nodes[i].position, nodeDiameter/2.0, nodes[j].position, nodeDiameter/2.0)) {
-                  if (isMouseOverNode && isNodeSelected && !isNodeLocked) { 
-                    //Lock node so when moving, selection is kept.
-                    selectedIndex = i;
-                    isNodeLocked = true;
-                    break;
-                  }
-                  else {
-                    DrawCircleV(nodes[i].position, nodes[i].diameter, nodes[i].color);
-                    DrawText(TextFormat("%d", i + 1), nodes[i].position.x - 20, nodes[i].position.y - 15, 8, BLACK);
-                  }
-                }
+                break;
+              } else {
+                DrawCircleV(nodes[i].position, nodes[i].diameter,
+                            nodes[i].color);
+                DrawText(TextFormat("%d", i + 1), nodes[i].position.x - 20,
+                         nodes[i].position.y - 15, 8, BLACK);
               }
             }
+          }
+        }
 
-              if (isNodeDrawn && isSpaceFree) {
-                nodes[nodeIndex].color = nodeColors[colorIndex];
-                nodes[nodeIndex].diameter = nodeDiameter;
-                nodes[nodeIndex].position = mousePosition;
-                printf("%d coordinates: %f, %f \n", nodeIndex, nodes[nodeIndex].position.x, nodes[nodeIndex].position.y);
-                nodeIndex++;
-                colorIndex++;
-                isNodeDrawn = false;
-              }
+        //Create Node
+        if (isNodeDrawn && isSpaceFree) {
+          nodes[nodeIndex].id = structCount;
+          nodes[nodeIndex].color = nodeColors[colorIndex];
+          nodes[nodeIndex].diameter = nodeDiameter;
+          nodes[nodeIndex].position = mousePosition;
+          printf("%d coordinates: %f, %f \n", nodeIndex,
+                 nodes[nodeIndex].position.x, nodes[nodeIndex].position.y);
+          nodeIndex++;
+          colorIndex++;
+          structCount++;
+          isNodeDrawn = false;
+        }
 
-            if (isNodeSelected) {
-              nodes[selectedIndex].position = mousePosition;
-              DrawCircleV(nodes[selectedIndex].position, nodes[selectedIndex].diameter, nodes[selectedIndex].color);
-              DrawText(TextFormat("%d", selectedIndex + 1), nodes[selectedIndex].position.x - 20, nodes[selectedIndex].position.y - 15, 8, BLACK);
-            }
+        //Move Node
+        if (isNodeSelected) {
+          nodes[selectedIndex].position = mousePosition;
+          DrawCircleV(nodes[selectedIndex].position,
+                      nodes[selectedIndex].diameter,
+                      nodes[selectedIndex].color);
+          DrawText(TextFormat("%d", selectedIndex + 1),
+                   nodes[selectedIndex].position.x - 20,
+                   nodes[selectedIndex].position.y - 15, 8, BLACK);
+          
+        }
 
-            //LINES---------------------
-            if (isLineStarted) {
-              lines[lineIndex].startNode = nodes[selectedIndex];
-              DrawLine(lines[lineIndex].startNode.position.x, lines[lineIndex].startNode.position.y, mousePosition.x, mousePosition.y, BLACK);
-            } else if (isLineEnded) {
-              lines[lineIndex].endNode = nodes[selectedIndex];
-              DrawLine(lines[lineIndex].startNode.position.x, lines[lineIndex].startNode.position.y, lines[lineIndex].endNode.position.x, lines[lineIndex].endNode.position.y, BLACK);
-              lineIndex++;
-              isLineStarted = false;
-              isLineEnded = false;
-              isNodeLocked = false;
-              numOfLines++;
-            }
+        // LINES---------------------
+        if (isNodeSelected) {
+          for (int k = 0; k < numOfLines; ++k) {
+            //Move lines too
+            if (nodes[selectedIndex].id == lines[k].startNode.id)
+              lines[k].startNode.position = nodes[selectedIndex].position;
+            if (nodes[selectedIndex].id == lines[k].endNode.id)
+              lines[k].endNode.position = nodes[selectedIndex].position;
+          }
+        }
 
-            printf("line %d coordinates: %f, %f to %f, %f \n", lineIndex-1, lines[lineIndex-1].startNode.position.x, lines[lineIndex-1].startNode.position.y, lines[lineIndex-1].endNode.position.x, lines[lineIndex-1].endNode.position.y);
-            printf("num of lines = %d", numOfLines);
+        if (isLineStarted) {
+          lines[lineIndex].startNode = nodes[selectedIndex];
+          DrawLine(lines[lineIndex].startNode.position.x,
+                   lines[lineIndex].startNode.position.y, mousePosition.x,
+                   mousePosition.y, BLACK);
+        } else if (isLineEnded) {
+          lines[lineIndex].endNode = nodes[selectedIndex];
+          DrawLine(lines[lineIndex].startNode.position.x,
+                   lines[lineIndex].startNode.position.y,
+                   lines[lineIndex].endNode.position.x,
+                   lines[lineIndex].endNode.position.y, BLACK);
+          lineIndex++;
+          isLineStarted = false;
+          isLineEnded = false;
+          isNodeLocked = false;
+          numOfLines++;
+        }
 
-            //draw lines
-            for (int k = 0; k < numOfLines; k++) {
-              DrawLine(lines[k].startNode.position.x, lines[k].startNode.position.y, lines[k].endNode.position.x, lines[k].endNode.position.y, BLACK);
-            }
-            DrawText("Graph Maker", 10, 10, 20, DARKGRAY);
+        printf("line %d coordinates: %f, %f to %f, %f \n", lineIndex - 1,
+               lines[lineIndex - 1].startNode.position.x,
+               lines[lineIndex - 1].startNode.position.y,
+               lines[lineIndex - 1].endNode.position.x,
+               lines[lineIndex - 1].endNode.position.y);
+        printf("num of lines = %d\n", numOfLines);
+
+        // draw lines
+        for (int k = 0; k < numOfLines; k++) {
+          DrawLine(lines[k].startNode.position.x, lines[k].startNode.position.y,
+                   lines[k].endNode.position.x, lines[k].endNode.position.y,
+                   BLACK);
+        }
+
+        DrawText("Graph Maker", 10, 10, 20, DARKGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -164,7 +203,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
