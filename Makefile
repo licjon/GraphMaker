3443d3b1be5c282c13709d0a608ccb4dc13ef6a6
Jon Licjon <licjon@protonmail.com>
2023-03-20 14:44:32 -0400

    Fix background color bug

--- Makefile
+++ Makefile
@@ -10,3 +10,6 @@ OBJ_NAME = main
 
 all : $(OBJS)
 	$(CC) $(OBJS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
+
+clean:
+	rm $(OBJS) main
--- graphs.c
+++ graphs.c
@@ -1,19 +1,10 @@
 #include "raylib.h"
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <glib.h>///Getting around this in Makefile
-#include <assert.h>
 
 #define MAXNODES 20
 
-GList* list = NULL;
 //------------------------------------------------------------------------------------
-void logError (int line, char* message)
-{
-  printf("[ %d %s]\n", line, message);
-}
-#define LOG( message ) logError( __LINE__, message )
 
 int main(void)
 {
@@ -25,50 +16,29 @@ int main(void)
 
     Vector2 mousePosition = { -100.0f, -100.0f };
 
-    typedef struct node {
+    struct node {
       int id;
       Vector2 position;
       int diameter;
       Color color;
-    } Node;
-
-    struct nodes {
-      Node node;
-      LIST_ENTRY(nodes) nodes;
-    };
-      
-    LIST_HEAD(nodehead, nodes);
-    struct nodes *currentNode;
-    struct nodehead head;
-    LIST_INIT(&head);
-    currentNode = malloc(sizeof (struct nodes));
-
-    typedef struct line {
-      Node startNode;
-      Node endNode;
-    } Line;
-
-    struct lines {
-      Line line;
-      LIST_ENTRY(lines) lines;
     };
 
+    struct line {
+      struct node startNode;
+      struct node endNode;
+    };
 
-    /* struct line lines[MAXNODES]; */
-    /* int lineIndex = 0; */
-    struct lines *currentLine;
+    struct line lines[MAXNODES];
+    int lineIndex = 0;
 
-    /* struct node nodes[MAXNODES]; */
-    int structCount = 1;
-    Node selectedNode;
-    Node newNode;
-    
-    /* int nodeIndex = 0; */
+    struct node nodes[MAXNODES];
+    int structCount = 0;
+    int nodeIndex = 0;
     int nodeDiameter = 10;
     bool isNodeDrawn = false;
     bool isNodeSelected = false;
     bool isNodeLocked = false;
-    /* int selectedIndex; */
+    int selectedIndex;
     Color nodeColors[MAXNODES] = {DARKBLUE, RED,   DARKGREEN,  ORANGE,  PINK,
                                   PURPLE,   BLACK, BROWN,      GOLD,    MAROON,
                                   YELLOW,   GRAY,  MAGENTA,       LIME,    SKYBLUE,
@@ -78,10 +48,7 @@ int main(void)
     bool isLineStarted = false;
     bool isLineEnded = false;
     int numOfLines = 0;
-
-    //Node list
-    //Line list
-
+    struct node startNode;
 
     SetTargetFPS(60);     
     //---------------------------------------------------------------------------------------
@@ -113,33 +80,27 @@ int main(void)
         bool isSpaceFree = true;
         bool isMouseOverNode = false;
 
-        struct nodes *n, *n2, *nodeStruct;
-
-        //Different things to do when mouse is over a node
-        for (n = LIST_FIRST(&head); n != NULL; LIST_NEXT(n, nodes)) {
+        for (int i = 0; i < MAXNODES; i++) {
           isMouseOverNode = CheckCollisionPointCircle(
-              mousePosition, n->node.position, nodeDiameter / 1.5);
+              mousePosition, nodes[i].position, nodeDiameter / 1.5);
 
           if (isMouseOverNode && !(isLineStarted || isLineEnded)) {
             isSpaceFree = false;
             isNodeDrawn = false;
           }
           if (isMouseOverNode && isLineStarted && !isNodeLocked) {
-            nodeStruct = n;
+            selectedIndex = i;
             isNodeLocked = true;
           } else if (isMouseOverNode && isLineEnded && isNodeLocked) {
-            /* startNode = lines[lineIndex].startNode; */
-            selectedNode = n->node;
+            selectedIndex = i;
           }
 
-
-          for (n2 = LIST_FIRST(&head); n2 != NULL; LIST_NEXT(n2, nodes)) {
-            if (!CheckCollisionCircles(n->node.position, nodeDiameter / 2.0,
-                                       n2->node.position, nodeDiameter / 2.0)) {
+          for (int j = 0; j < MAXNODES; j++) {
+            if (!CheckCollisionCircles(nodes[i].position, nodeDiameter / 2.0,
+                                       nodes[j].position, nodeDiameter / 2.0)) {
               if (isMouseOverNode && isNodeSelected && !isNodeLocked) {
                 // Lock node so when moving, selection is kept.
-                selectedNode = n->node;
-                nodeStruct = n;
+                selectedIndex = i;
                 isNodeLocked = true;
                 break;
               }
@@ -148,21 +109,38 @@ int main(void)
         }
 
         //Create Node
-        if (isNodeDrawn && isSpaceFree) {
-
-          newNode.id = structCount;
-          newNode.color = nodeColors[colorIndex];
-          newNode.diameter = nodeDiameter;
-          newNode.position = mousePosition;
-          currentNode->node = newNode;
-          LIST_INSERT_HEAD(&head, currentNode, nodes);
+        if (isNodeDrawn && isSpaceFree && structCount <= MAXNODES - 1) {
+          nodes[nodeIndex].id = structCount;
+          nodes[nodeIndex].color = nodeColors[colorIndex];
+          nodes[nodeIndex].diameter = nodeDiameter;
+          nodes[nodeIndex].position = mousePosition;
+          printf("%d coordinates: %f, %f \n", nodeIndex,
+                 nodes[nodeIndex].position.x, nodes[nodeIndex].position.y);
+          nodeIndex++;
           colorIndex++;
           structCount++;
           isNodeDrawn = false;
         }
 
-<<<<<<< HEAD
-=======
+
+        // LINES---------------------
+        if (isNodeSelected) {
+          for (int k = 0; k < numOfLines; ++k) {
+            //Move lines too
+            if (nodes[selectedIndex].id == lines[k].startNode.id)
+              lines[k].startNode.position = nodes[selectedIndex].position;
+            if (nodes[selectedIndex].id == lines[k].endNode.id)
+              lines[k].endNode.position = nodes[selectedIndex].position;
+          }
+        }
+        //----------------------------------------------------------------------------------
+        
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        
+        ClearBackground(RAYWHITE);
+        
         //Move Node
         if (isNodeSelected) {
           nodes[selectedIndex].position = mousePosition;
@@ -175,69 +153,26 @@ int main(void)
           
         }
 
->>>>>>> parent of a49c82f (Moved a bit of code)
-        // LINES---------------------
-
-        struct lines *ln;
-        if (isNodeSelected) {
-          for (ln = STAILQ_FIRST(&lhead); ln != NULL; STAILQ_NEXT(ln, lines)) {
-            //Move lines too
-            if (selectedNode.id == ln->line.startNode.id)
-              ln->line.startNode.position = selectedNode.position;
-            if (selectedNode.id == ln->line.endNode.id)
-              ln->line.endNode.position = selectedNode.position;
-          }
-        }
-        //----------------------------------------------------------------------------------
-        
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-        
-        ClearBackground(RAYWHITE);
-        
-<<<<<<< HEAD
-        //Move Node
-        if (isNodeSelected) {
-          selectedNode.position = mousePosition;
-          DrawCircleV(selectedNode.position,
-                      selectedNode.diameter,
-                      selectedNode.color);
-          DrawText(TextFormat("%d", selectedNode.id),
-                   selectedNode.position.x - 20,
-                   selectedNode.position.y - 15, 8, BLACK);
-        }
-
-        
-        struct nodes *n3;
-        for (n3 = STAILQ_FIRST(&head); n3 != NULL; STAILQ_NEXT(n3, nodes)) {
-            DrawCircleV(n3->node.position, n3->node.diameter,
-                        n3->node.color);
-            DrawText(TextFormat("%d", n3->node.id), n3->node.position.x - 20,
-                     n3->node.position.y - 15, 8, BLACK);
-=======
-        for (int i = 0; i < MAXNODES; i++) {
-          for (int j = 0; j < MAXNODES; j++) {
+        for (int i = 0; i < nodeIndex; i++) {
             DrawCircleV(nodes[i].position, nodes[i].diameter,
                         nodes[i].color);
             DrawText(TextFormat("%d", i + 1), nodes[i].position.x - 20,
                      nodes[i].position.y - 15, 8, BLACK);
->>>>>>> parent of a49c82f (Moved a bit of code)
-          }
+        }
 
         //Draw line before it is connected to end node
         if (isLineStarted) {
-          currentLine->line.startNode = selectedNode;
-          DrawLine(currentLine->line.startNode.position.x,
-                   currentLine->line.startNode.position.y, mousePosition.x,
+          lines[lineIndex].startNode = nodes[selectedIndex];
+          DrawLine(lines[lineIndex].startNode.position.x,
+                   lines[lineIndex].startNode.position.y, mousePosition.x,
                    mousePosition.y, BLACK);
         } else if (isLineEnded) {
-          currentLine->line.endNode = selectedNode;
-          DrawLine(currentLine->line.startNode.position.x,
-                   currentLine->line.startNode.position.y,
-                   currentLine->line.endNode.position.x,
-                   currentLine->line.endNode.position.y, BLACK);
-          STAILQ_NEXT(currentLine, lines);
+          lines[lineIndex].endNode = nodes[selectedIndex];
+          DrawLine(lines[lineIndex].startNode.position.x,
+                   lines[lineIndex].startNode.position.y,
+                   lines[lineIndex].endNode.position.x,
+                   lines[lineIndex].endNode.position.y, BLACK);
+          lineIndex++;
           isLineStarted = false;
           isLineEnded = false;
           isNodeLocked = false;
@@ -245,11 +180,9 @@ int main(void)
         }
 
         // draw lines
-        
-        struct lines *ln2;
-          for (ln2 = STAILQ_FIRST(&lhead); ln2 != NULL; STAILQ_NEXT(ln2, lines)) {
-          DrawLine(ln2->line.startNode.position.x, ln2->line.startNode.position.y,
-                   ln2->line.endNode.position.x, ln2->line.endNode.position.y,
+        for (int k = 0; k < numOfLines; k++) {
+          DrawLine(lines[k].startNode.position.x, lines[k].startNode.position.y,
+                   lines[k].endNode.position.x, lines[k].endNode.position.y,
                    BLACK);
         }
 
